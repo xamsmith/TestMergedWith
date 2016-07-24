ef657edc634a684e5f1bac79f7f21d8c3002ef8b
Michael Smith <lvmike099@gmail.com>
2016-07-23 20:26:14 -0700

    Add App.xaml and MainPage.xaml instead of App.cs

---
+++ TestMergedWith/TestMergedWith/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace TestMergedWith
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new MainPage();
+        }
+    }
+}
